d3b2dc2445e1bd4ab87546c93968e02b7fbdbc7b
taufiqalfianto <91525251+taufiqalfianto@users.noreply.github.com>
2021-10-28 17:23:33 +0700

    Add files via upload

---
+++ DayUserUnitTesting/DayUserUnitTest/DayUserTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace DayUserUnitTest
+{
+    [TestFixture]
+    public class DayUserTest
+    {
+        private Day day;
+        private User user;
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+
+        }
+
+        [Test]
+        public void NameOfDayTest()
+        {
+            Assert.AreEqual("Rabu", day.NameOfDay(3));
+        }
+        [Test]
+        public void IsValidUserTest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+        }
+    }
+}
